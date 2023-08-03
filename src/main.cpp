930952473f37b3a41d09db49a532a0ced565ef48
Nicolas Fella <nicolas.fella@gmx.de>
2023-08-03 12:24:29 +0200

    Drop Android system bar recoloring
    It doesn't build against Qt6 and is a somewhat unimportant gimmick

--- src/main.cpp
+++ src/main.cpp
@@ -25,15 +25,7 @@
 #include "InfoData.h"
 
 #ifdef Q_OS_ANDROID
-#include <QtAndroid>
 #include <QGuiApplication>
-
-// WindowManager.LayoutParams
-#define FLAG_TRANSLUCENT_STATUS 0x04000000
-#define FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS 0x80000000
-// View
-#define SYSTEM_UI_FLAG_LIGHT_STATUS_BAR 0x00002000
-
 #else
 #include <QApplication>
 #endif
@@ -75,17 +67,6 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
     if (engine.rootObjects().isEmpty()) {
         return -1;
     }
-
-    //HACK to color the system bar on Android, use qtandroidextras and call the appropriate Java methods 
-#ifdef Q_OS_ANDROID
-    QtAndroid::runOnAndroidThread([=]() {
-        QAndroidJniObject window = QtAndroid::androidActivity().callObjectMethod("getWindow", "()Landroid/view/Window;");
-        window.callMethod<void>("addFlags", "(I)V", FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        window.callMethod<void>("clearFlags", "(I)V", FLAG_TRANSLUCENT_STATUS);
-        window.callMethod<void>("setStatusBarColor", "(I)V", QColor("#2196f3").rgba());
-        window.callMethod<void>("setNavigationBarColor", "(I)V", QColor("#2196f3").rgba());
-    });
-#endif
 
     return app.exec();
 }
